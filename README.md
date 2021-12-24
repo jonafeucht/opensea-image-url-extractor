f7465382a82c511d5873125c874fecb2c1aa4e9d
JONA T. FEUCHT <jona@keemail.me>
2021-12-24 08:54:22 +0100

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Opensea.io image URL extractor.
+
+Purpose of this script, is to help artists get the URLs for their stolen art and report it to Google.
+
+- 1. Go to network tab, refresh the page, wait for images to load (on some sites you may have to scroll down to the images for them to start loading).
+- 2. Right click/ctrl click on any entry in the network log, and click on ”Save all as HAR with content”.
+- 3. Run script.
---
+++ opensea.py
@@ -0,0 +1,16 @@
+import json
+from haralyzer import HarParser, HarPage
+
+with open('opensea.io.har', 'r') as f:
+    har_parser = HarParser(json.loads(f.read()))
+
+data = har_parser.har_data["entries"]
+image_urls = []
+
+for entry in data:
+    if entry["response"]["content"]["mimeType"].find("image/") == 0:
+        image_urls.append(entry["request"]["url"])
+     
+with open('target_link.txt', 'w') as f:
+    for link in image_urls:
+        f.write("%s\n" % link)
